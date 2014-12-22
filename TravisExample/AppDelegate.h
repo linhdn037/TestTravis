3173270015c4a67c2b3bfdd7b02b138d937fac95
linhdn037 <linhdonhu@gmail.com>
2014-12-22 11:43:19 +0700

    test 4

---
+++ TravisExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravisExample
+//
+//  Created by Linhdn on 12/22/14.
+//  Copyright (c) 2014 Linhdn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravisExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisExample
+//
+//  Created by Linhdn on 12/22/14.
+//  Copyright (c) 2014 Linhdn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
